dd23e5ea2982034b7a67f1fa1266c6859d80dd82
Emma Worley <emma@emma.gg>
2025-08-20 14:49:40 -0700

    Bump parcel

---
+++ src/common/disableParcelErrorOverlay.ts
@@ -0,0 +1,9 @@
+/* tslint:disable:no-empty */
+
+// TODO: remove when parcel has a flag for this (https://github.com/parcel-bundler/parcel/issues/9738)
+
+export function setEditorHandler() {}
+export function reportRuntimeError() {}
+export function startReportingRuntimeErrors() {}
+export function dismissRuntimeErrors() {}
+export function stopReportingRuntimeErrors() {}
