7706f160821d69e1fadd2ff00571c1352ce2c1d2
Emma Worley <emma@emma.gg>
2025-10-11 11:16:43 -0700

    Fix Service-Worker-Allowed scope

--- src/main/middleware/remoconServiceWorkerAllowed.ts
+++ src/main/middleware/remoconServiceWorkerAllowed.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 
 function remoconServiceWorkerAllowed() {
   return (req: Request, res: Response, next: () => void) => {
-    res.append("Service-Worker-Allowed", "/notificationServiceWorker.js");
+    res.append("Service-Worker-Allowed", "/");
     next();
   };
 }
